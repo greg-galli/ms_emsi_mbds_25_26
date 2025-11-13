e94086db217539a72518c5f27c26da115c9d45eb
Gregory Galli <greg.galli@gmail.com>
2025-11-13 21:17:03 +0100

    Added Actuator & Resilience4J

--- account/src/main/java/fr/miage/mbds/account/CustomerClient.java
+++ account/src/main/java/fr/miage/mbds/account/CustomerClient.java
@@ -1,17 +1,32 @@
 package fr.miage.mbds.account;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "customer", url = "http://localhost:8080")
+@FeignClient(name = "CUSTOMER")
 public interface CustomerClient {
 
     @GetMapping("/customers/{customerId}")
-    public CustomerProxy getCustomerById(@PathVariable Long customerId);
+    @CircuitBreaker( name = "customer", fallbackMethod = "getDefaultCustomer")
+    CustomerProxy getCustomerById(@PathVariable Long customerId);
 
     @GetMapping("/customers")
-    public List<CustomerProxy> getCustomers();
+    @CircuitBreaker( name = "customers", fallbackMethod = "getDefaultCustomerList")
+    List<CustomerProxy> getCustomers();
+
+    default CustomerProxy getDefaultCustomer(Long customerId, Exception exception) {
+        return CustomerProxy.builder()
+                .firstName("Indisponible")
+                .lastName("Indisponible")
+                .email("Indisponible")
+                .build();
+    }
+
+    default List<CustomerProxy> getDefaultCustomerList(Exception exception) {
+        return List.of();
+    }
 }
--- account/src/main/java/fr/miage/mbds/account/CustomerProxy.java
+++ account/src/main/java/fr/miage/mbds/account/CustomerProxy.java
@@ -1,8 +1,10 @@
 package fr.miage.mbds.account;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CustomerProxy {
     private Long id;
     private String firstName;
