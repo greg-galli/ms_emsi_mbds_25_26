74b2d7b4cda8cab172b635d29e8cd92b93f81b25
Gregory Galli <greg.galli@gmail.com>
2025-11-13 17:02:42 +0100

    Added Eureka + clients

---
+++ discovery/src/test/java/fr/miage/mbds/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.mbds.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
